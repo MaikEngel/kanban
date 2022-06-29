453cbdaf8d495949f39fb0e7fe624054e4f36c0c
Nareg-B-94 <naregb94@gmail.com>
2022-06-29 14:25:46 +0200

    nichts!

--- backlog.js
+++ backlog.js
@@ -5,9 +5,9 @@ function addBacklog() {
     let backlog = document.getElementById('backlogItem')
     backlog.innerHTML = ``;
     console.log (backlog);
-    for (let i = 0; i < 8; i++) {
-        let task=tasks[i];
-        backlog.innerHTML += `
+    for (let i = 0; i < tasks.length; i++) {
+    let task = tasks[i];
+    backlog.innerHTML += `
         <div id="backlogContainer${i}" class="backlogContainer urgencyColorRed">
                 <div class="assignedTo" class="category">
                     <img src="img/guy.jpg" alt="" class="backlogAssignedToPicture" />
@@ -16,13 +16,14 @@ function addBacklog() {
                         <p class="fontColor margin0">Email</p>
                     </div>
                 </div>
-                <p class="category">${task["catergory"]}</p>
+                <p class="category">${task["category"]}</p>
                 <div class="detailsContainer">
                     <h3 class="fontColor detailsHeadlineResponsiv">DETAILS</h3>
-                    <p class="details">
-                    created at:&nbsp${task["createdAt"]} <br>
-                        ${task["description"]}.
-                    </p>
+                    Created at: &nbsp ${task["createdAt"]}. <br>
+                    Urgency &nbsp: &nbsp ${task["urgency"]}.
+                    <div class="descriptions">
+                    Desc. &nbsp: &nbsp ${task["description"]}. <br>
+                    </div>
                 </div>
             </div>
         </div>
