4e7a033fd7fcc1c88a05960dc3bbe9caaf71a721
Nareg-B-94 <naregb94@gmail.com>
2022-06-30 11:59:16 +0200

    urgency Level funktioniert

--- backlog.js
+++ backlog.js
@@ -1,12 +1,10 @@
 function addBacklog() {
-    let backlog = document.getElementById('backlogItem')
-    backlog.innerHTML = ``;
-    for (let i = 0; i < tasks.length; i++) {
-        let task=tasks[i]
+  let backlog = document.getElementById("backlogItem");
+  backlog.innerHTML = ``;
+  for (let i = 0; i < tasks.length; i++) {
+    let task = tasks[i];
 
-
-
-        backlog.innerHTML += `
+    backlog.innerHTML += `
         <div id="backlogContainer${i}" class="backlogContainer ">
                 <div class="assignedTo" class="category">
                     <img src=${task["selectetAvatar"]["picture"]} alt="" class="backlogAssignedToPicture" />
@@ -18,31 +16,23 @@ function addBacklog() {
                 <div class="detailsContainer">
                     <h3 class="fontColor detailsHeadlineResponsiv">DETAILS</h3>
                     <p class="details">
-                        ${task['description']}.
+                        ${task["description"]}.
                     </p>
                 </div>
             </div>
         </div>
         `;
-let bgColor = document.getElementById("backlogContainer" + i);
-let urgencyLevel = task['urgency'];
+    let bgColor = document.getElementById("backlogContainer" + i);
+    let urgencyLevel = task["urgency"];
 
-if (urgencyLevel == "high") {
-  bgColor.classList.add('urgencyColorRed');
+    if (urgencyLevel == "high") {
+      bgColor.classList.add("urgencyColorRed");
+    }
+    if (urgencyLevel == "medium") {
+      bgColor.classList.add("urgencyColorBlue");
+    }
+    if (urgencyLevel == "low") {
+      bgColor.classList.add("urgencyColorOrange");
+    }
+  }
 }
-if (urgencyLevel == "medium") {
-  bgColor.classList.add('urgencyColorBlue');
-}
-if (urgencyLevel == "low") {
-  bgColor.classList.add('urgencyColorOrange');
-}
-
-}
-
-
-
-
-}
-
-
-
