621f7237b3afa2406c58f39f28946250216798ae
Maik Engel <maik.engel7@gmail.com>
2022-07-01 09:57:52 +0200

    try it now

--- board.js
+++ board.js
@@ -52,11 +52,7 @@ function updateTasks() {
 
 function generateTodoHTML(task) {
   return `
-<<<<<<< HEAD
 <div class="taskContainer" draggable="true" ondragstart="dragTask(${task['id'] - 1})" onclick="openFullscreen(${task['id'] - 1})">
-=======
-<div class="taskContainer" draggable="true" ondragstart="dragTask(${task['id'] - 1})">
->>>>>>> 48564793d032b5461624813673e904f4c0213b8e
   <div class="taskHeadline">
     <span style="margin-bottom: 8px; font-size: 20px;">${task['title']}</span>
   </div>  
@@ -98,22 +94,22 @@ function dropTask(category) {
   updateTasks();
 }
 
-<<<<<<< HEAD
 function openFullscreen(id) {
   let fullscreen = document.getElementById('fullscreen');
   fullscreen.classList.remove('dNone')
   let task = tasks[id]
   fullscreen.innerHTML = ``;
   fullscreen.innerHTML = `
-    <div class="fullscreenTask">
+    <div class="fullscreenTask" id="fullscreenTask">
+    <img class="fullscreenTaskPicture" src="${task['selectetAvatar']['picture']}">
       <div class="fullscreenTaskHeadline">
         <h2 class="fontColor">${task['title']}</h2>
-        <span style="margin-left: 8px;">Urgency</span>
       </div><br>
       <div style="display: flex; flex-direction: column;">
-        <div>
-          <span style="display: flex; align-items: center;">Assigned to: ${task['selectetAvatar']['name']}, ${task['selectetAvatar']['email']} <img class="taskPicture" src="${task['selectetAvatar']['picture']}"></span><br>
-        </div>  
+        <div >
+          <span>Assigned to: ${task['selectetAvatar']['name']}</span><br>
+          <span>E-Mail: ${task['selectetAvatar']['email']}</span>
+        </div><br>  
         <span>${task['catergory']}</span>
       </div><br>
       <div>
@@ -121,128 +117,23 @@ function openFullscreen(id) {
         <div class="fullscreenTaskDescription">
           <span>${task['description']}</span>
         </div>
-      </div><br>
+      </div>
       <div style="display: flex; justify-content: space-between;">
         <span>Created at: ${task['createdAt']}</span>
         <span>Due date: ${task['date']}</span>
       </div>
     </div>
   `;
-
+  urgencyFullscreenColor(task)
 }
-=======
-// function moveTo()
 
-// function drag(ev) {
-//   ev.dataTransfer.setData("text", ev.target.id);
-// }
-
-// function drop(ev) {
-//   ev.preventDefault();
-//   var data = ev.dataTransfer.getData("text");
-//   ev.target.appendChild(document.getElementById(data));
-// }
-
-
-// let taskColumns = Array.from(document.querySelectorAll(".column"));
-// let boardTasks = [...document.querySelectorAll(".task-cont")];
-// console.log("board.js loaded");
-// async function tasksToBoard() {
-//   tasks = backend.getItem("tasks");
-//   tasks.forEach((task) => {
-//     //task elements
-//     let taskCont = document.createElement("div");
-//     taskCont.classList.add("task-cont");
-//     taskCont.id = "task-id" + task.id;
-//     let taskTitle = document.createElement("h4");
-//     taskTitle.classList.add("task-title");
-//     taskTitle.textContent = task.title;
-//     let taskDescription = document.createElement("p");
-//     taskDescription.classList.add("task-description");
-//     taskDescription.textContent = task.description;
-//     //taskID
-
-//     //overflow toggle
-//     let overflowToggle = document.createElement("span");
-//     overflowToggle.textContent = "^";
-//     overflowToggle.classList.add("overflow-toggle", "rotated");
-//     overflowToggle.addEventListener("click", wholeTaskVisible);
-//     //draggable
-//     taskColumns.forEach((column) =>
-//       column.addEventListener("dragover", (ev) => {
-//         ev.preventDefault();
-//         column.addEventListener("drop", taskDrop);
-//         // this.style.cursor = "pointer";
-//       })
-//     );
-//     taskCont.draggable = true;
-//     taskCont.addEventListener("dragstart", taskDragStart);
-//     taskCont.append(taskTitle, taskDescription, overflowToggle);
-//     // taskCont.addEventListener("pointerover", textResizeGrow);
-//     // taskCont.addEventListener("pointerleave", textResizeShrink);
-//     taskColumns
-//       .find((column) => column.id === task.boardColumn)
-//       .append(taskCont);
-//   });
-// }
-// async function initBoard() {
-//   await downloadFromServer();
-//   tasksToBoard();
-//   boardTasks = [...document.querySelectorAll(".task-cont")];
-// }
-// initBoard();
-// function textResizeGrow(ev) {
-//   let colChildren = this.parentElement.children;
-
-//   console.log();
-//   [...colChildren].forEach(
-//     (child) =>
-//       (child.style.fontSize =
-//         parseInt(getComputedStyle(this.parentElement).width) / 13 + "px")
-//   );
-// }
-
-// function textResizeShrink(ev) {
-//   let colChildren = this.parentElement.children;
-//   console.log();
-//   [...colChildren].forEach(
-//     (child) =>
-//       (child.style.fontSize =
-//         parseInt(getComputedStyle(this.parentElement).width) / 14 + "px")
-//   );
-// }
-// function wholeTaskVisible(ev) {
-//   //   this.parentElement.style.height = "1000px";
-//   this.classList.toggle("rotated");
-//   this.parentElement.classList.toggle("show-everything");
-//   this.parentElement.parentElement.classList.toggle("overflow-scroll");
-//   this.scrollIntoView();
-// }
-
-// function taskDragStart(ev) {
-//   //   ev.preventDefault();
-//   ev.dataTransfer.setData("text", this.id);
-// }
-// function taskDrop(ev) {
-//   //   ev.preventDefault();
-//   //   console.log(ev.type);
-//   ev.preventDefault();
-//   let transferId = ev.dataTransfer.getData("text");
-//   this.append(document.getElementById(transferId));
-//   setTaskCategories();
-// }
-// async function setTaskCategories() {
-//   let tasksCopy = JSON.parse(JSON.stringify(tasks));
-//   console.log(tasksCopy);
-//   [...document.querySelectorAll(".task-cont")].forEach((task) => {
-//     tasksCopy.find(
-//       (clonedTask) => clonedTask.id === parseInt(task.id.replace(/[^0-9]/g, ""))
-//     ).boardColumn = task.parentElement.id;
-//   });
-//   console.log(tasksCopy);
-//   tasksCopy.forEach((entry) => console.log(entry.title, entry.boardColumn));
-//   backend.setItem("tasks", tasksCopy).then(() => {
-//     console.log("tasks saved");
-//   });
-// }
->>>>>>> 48564793d032b5461624813673e904f4c0213b8e
+function urgencyFullscreenColor(task) {
+  let backgroundColor = document.getElementById('fullscreenTask')
+  if (task['urgency'] == "high") {
+    backgroundColor.classList.add('urgencyFullscreenColorRed')
+  } if (task['urgency'] == "medium") {
+    backgroundColor.classList.add('urgencyFullscreenColorOrange')
+  } if (task['urgency'] == "low") {
+    backgroundColor.classList.add('urgencyFullscreenColorBlue')
+  }
+}
